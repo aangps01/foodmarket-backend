6513aa405e4f6df114c07bd6854e4451e61f15dc
Aang Pangantyas Sampurna <aangpangantyas@gmail.com>
2022-05-18 01:23:03 +0800

    add registration api

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -6,6 +6,8 @@ use Illuminate\Http\Request;
 use App\Helpers\ResponseFormatter;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\API\UserLoginRequest;
+use App\Http\Requests\API\UserRegisterRequest;
+use App\Models\User;
 use Exception;
 use Illuminate\Support\Facades\Auth;
 
@@ -34,6 +36,27 @@ class UserController extends Controller
             return ResponseFormatter::error([
                 'message' => 'Something Went Wrong'
             ], 'Authentication Failed', 500);
+        }
+    }
+
+    public function register(UserRegisterRequest $request)
+    {
+        try {
+            $validated = $request->validated();
+
+            User::create($validated);
+            $user = User::where('email', $validated['email'])->first();
+            $token = $user->createToken('AuthToken')->plainTextToken;
+
+            return ResponseFormatter::success([
+                'access_token' => $token,
+                'token_type' => 'Bearer',
+                'user' => $user
+            ], 'Registration Successfully');
+        } catch (Exception $e) {
+            return ResponseFormatter::error([
+                'message' => 'Something Went Wrong'
+            ], 'Registration Failed', 500);
         }
     }
 }
